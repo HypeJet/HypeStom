cfbff48e9335195f7ce29ee8bec10818066db1e2
super <superjojominecraft@gmail.com>
2022-12-11 16:51:41 +0100

    🟠 Add Location

--- src/main/java/me/heroostech/citystom/listener/Listener.java
+++
@@ -1,8 +0,0 @@
-package me.heroostech.citystom.listener;
-
-import net.minestom.server.event.Event;
-import net.minestom.server.event.EventNode;
-
-public interface Listener<E extends Event> {
-    EventNode<E> events();
-}
