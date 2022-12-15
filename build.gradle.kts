365d7d4c187d028e401fd8acb83ea268b2e73b84
super <superjojominecraft@gmail.com>
2022-12-15 15:13:11 +0100

    🟠 Back local publication

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id ("com.github.johnrengelman.shadow") version "7.1.0"
 }
 
-group = "me.heroostech.citystom"
+group = "xyz.citywide.citystom"
 version = "v1.0.0"
 
 repositories {
@@ -22,6 +22,21 @@ dependencies {
 }
 
 project.tasks.findByName("jar")?.enabled = false
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            afterEvaluate {
+                val shadowJar = tasks.findByName("shadowJar")
+                if (shadowJar == null) from(components["java"])
+                else artifact(shadowJar)
+            }
+            groupId = "me.heroostech.citystom"
+            artifactId = "CityStom"
+            version = "v1.0.0"
+        }
+    }
+}
 
 tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
     mergeServiceFiles()
