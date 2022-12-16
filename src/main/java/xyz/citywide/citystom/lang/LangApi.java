7b93868f5dae2ef74654bc635982f35e93046bd5
super <superjojominecraft@gmail.com>
2022-12-16 16:00:30 +0100

    Untested

--- src/main/java/xyz/citywide/citystom/lang/LangApi.java
+++ src/main/java/xyz/citywide/citystom/lang/LangApi.java
@@ -1,13 +1,26 @@
 package xyz.citywide.citystom.lang;
 
+import lombok.SneakyThrows;
+import net.minestom.server.entity.Player;
 import net.minestom.server.extensions.Extension;
+import org.jetbrains.annotations.NotNull;
 
+import java.sql.Connection;
 import java.util.Locale;
 
 public sealed interface LangApi permits LangApiImpl {
-    static LangApi create(Extension extension) {
-        return new LangApiImpl(extension);
+    @SneakyThrows
+    static LangApi create(@NotNull Extension extension, @NotNull Connection sqlConnection, @NotNull String table) {
+        return new LangApiImpl(extension, sqlConnection.createStatement(), table);
     }
 
-    String getString(Locale locale, String name);
+    String getString(@NotNull Locale locale, @NotNull String name);
+
+    Locale getLocale(@NotNull Player player);
+
+    boolean setLocale(@NotNull Player player, @NotNull Locale locale);
+
+    boolean setClientLocale(@NotNull Player player);
+
+    void createTable();
 }
